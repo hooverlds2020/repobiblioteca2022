136a3b5e51fc9b44b129b327beba9ac9ca4ffbb7
Roberto <hooverlds93@gmail.com>
2022-09-21 15:59:11 -0500

    Actualización de proyecto

--- biblioteca/apps/libro/views.py
+++ biblioteca/apps/libro/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,redirect
+from ast import Try
+from urllib import request
+from django.shortcuts import render, redirect
 from .forms import AutorForm
-
+from .models import Autor
 
 # Create your views here.
 
@@ -16,3 +18,22 @@ def crearAutor(request):
     else:
         autor_form = AutorForm()
     return render(request,'libro/crear_autor.html',{'autor_form':autor_form})
+
+def listarAutor(request):
+    autores = Autor.objects.all()
+    return render(request, 'libro/listar_autor.html',{'autores': autores})
+
+def editarAutor(request, id):
+    try:
+        autor = Autor.objects.get(id = id)
+        if request.method == 'GET':
+            autor_form = AutorForm(instance = autor)
+        else:
+            autor_form = AutorForm(request.POST, instance=autor)
+            if autor_form.is_valid():
+                autor_form.save()
+            return redirect('index')
+    except Exception as e:
+        raise
+    
+    return render(request, 'libro/crear_autor.html', {'autor_form':autor_form})
